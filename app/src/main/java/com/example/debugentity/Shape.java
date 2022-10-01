6a65407645c72b27792fa6f7da960ac5e6481d8b
7niyaij <90810668+7niyaij@users.noreply.github.com>
2022-10-01 17:47:12 -0600

    Update Shape.java

--- app/src/main/java/com/example/debugentity/Shape.java
+++ app/src/main/java/com/example/debugentity/Shape.java
@@ -3,5 +3,9 @@ package com.example.debugentity;
 abstract class Shape {
     int x;
     int y;
+// <<<<<<< HEAD
     String color = "black";
+// =======
+//    String color = "$PUT_YOUR_CHOICE_OF_COLOR";
+// >>>>>>> 7d6807b592a672c9340c328d0dc5f4614e08a94f
 }
